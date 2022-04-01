66506b4fbdb689e492e30856b234246a283609eb
cousseth <thomas.cousseau@reseau.eseo.fr>
2022-04-01 11:25:21 +0200

    Fonctions get post put delete format JSON

--- src/main/java/com/controller/VilleController.java
+++ src/main/java/com/controller/VilleController.java
@@ -1,5 +1,9 @@
 package com.controller;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +15,7 @@ import com.dao.VilleBDDImpl;
 @RestController
 public class VilleController {
 
+	// TODO : permettre de récupérer TOUTES les infos des villes
 	// fonction pour récupérer le contenu de la BDD
 	@RequestMapping(value = "/ville", method = RequestMethod.GET)
 	public String get(@RequestParam(required = false, value = "codePostal") String codePostals) {
@@ -18,13 +23,42 @@ public class VilleController {
 		return VilleBDDImpl.getVilles(codePostals);
 		
 	}
-
-	// TODO :
+	
+	// TODO : a modifier pour avoir le json to map
 	// fonction pour enregistrer un element dans la BDD
 	@RequestMapping(value = "/ville", method = RequestMethod.POST)
 	public String post(@RequestBody String requete) {
-		String[] listParams = requete.split("&");
-		return VilleBDDImpl.ajouterVilles(listParams);
+		try {
+			return VilleBDDImpl.ajouterVilles(VilleBDDImpl.jsonToMap(requete));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			return e.getMessage();
+		}
+
 		
+	}
+	
+	// TODO :
+	// fonction pour modifier un element dans la BDD
+	@RequestMapping(value = "/ville", method = RequestMethod.PUT)
+	public String put(@RequestBody String requete) {
+		try {
+			return VilleBDDImpl.modifVilleBDD(VilleBDDImpl.jsonToMap(requete));
+		} catch (IOException e) {
+			return e.getMessage();
+		}
+		
+	}
+	
+	
+	// TODO :
+	// fonction pour supprimer un element dans la BDD
+	@RequestMapping(value = "/ville", method = RequestMethod.DELETE)
+	public String delete(@RequestBody String requete) {
+		try {
+			return VilleBDDImpl.supprVilleBDD(VilleBDDImpl.jsonToMap(requete));
+		} catch (IOException e) {
+			return e.getMessage();
+		}
 	}
 }
--- src/main/java/com/dao/VilleBDDImpl.java
+++ src/main/java/com/dao/VilleBDDImpl.java
@@ -1,11 +1,18 @@
 package com.dao;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.modele.Commune;
 
 public class VilleBDDImpl {
@@ -16,6 +23,8 @@ public class VilleBDDImpl {
 		ResultSet resultat = null;
 		String villesDescription = "";
 		String requete = null;
+		Map<String, Object> map = new HashMap();
+		
 		try {
 			DaoFactory daoFactory = DaoFactory.getInstance();
 			connexion = daoFactory.getConnection();
@@ -29,8 +38,9 @@ public class VilleBDDImpl {
 				System.out.println("Requête envoyé");
 
 				while (resultat.next()) {
+					String codePostal = resultat.getString("Code_postal");
 					String nom_commune = resultat.getString("Nom_commune");
-					villesDescription += "<p>Nom commune : " + nom_commune + "</p>";
+					map.put(nom_commune, codePostal);
 				}
 			} else {
 				String[] listCodePostal = codePostals.split(",");
@@ -38,28 +48,41 @@ public class VilleBDDImpl {
 					requete = "SELECT * FROM ville_france WHERE Code_postal = '" + codePostal + "';";
 					statement = connexion.createStatement();
 					resultat = statement.executeQuery(requete);
-					System.out.println("Requête envoyé");
-
+					
 					while (resultat.next()) {
 						String nom_commune = resultat.getString("Nom_commune");
-						villesDescription += "<p>Nom commune : " + nom_commune + "</p>";
+						map.put(nom_commune, codePostal);
 					}
 				}
 			}
-
+			
+			connexion.close();
 		} catch (SQLException e) {
-			System.out.println(e.getMessage());
+			return e.getMessage();
 		}
-
+		
+		
+		try {
+			villesDescription =  new ObjectMapper().writeValueAsString(map);
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			return e.getMessage();
+		}
+		
+		
 		return villesDescription;
 	}
 
-	public static String ajouterVilles(String[] listParams) {
+	
+	
+	
+	
+	public static String ajouterVilles(Map<String, String> listModif) {
 		Connection connexion = null;
 		PreparedStatement preparedStatement = null;
 		Commune commune = new Commune();
 
-		affecteParamToCommune(commune, listParams);
+		affecteParamToCommune(commune, listModif);
 
 		try {
 			DaoFactory daoFactory = DaoFactory.getInstance();
@@ -76,49 +99,131 @@ public class VilleBDDImpl {
 			preparedStatement.setString(6, commune.getLatitude());
 			preparedStatement.setString(7, commune.getLongitude());
 			preparedStatement.executeUpdate();
-
+			
+			connexion.close();
 		} catch (SQLException e) {
 			return e.getMessage();
-		}
+		} 
 
 		return "Insertion passé en BDD";
 	}
 
 	
 	
-	private static void affecteParamToCommune(Commune commune, String[] listParams) {
-		for (String param : listParams) {
-			String key = param.split("=")[0];
-			String value = param.split("=")[1];
-			switch (key) {
+	
+	
+	
+	private static void affecteParamToCommune(Commune commune, Map<String, String> listModif) {
+		Object[] listKey = listModif.keySet().toArray();
+		for (Object key : listKey) {
+			switch ((String) key) {
 				case "Nom_commune":
-					commune.setNom_commune(value);
+					commune.setNom_commune(listModif.get(key));
 					break;
 	
 				case "Code_commune_INSEE":
-					commune.setCode_commune_INSEE(value);
+					commune.setCode_commune_INSEE(listModif.get(key));
 					break;
 	
 				case "Code_postal":
-					commune.setCode_postal(value);
+					commune.setCode_postal(listModif.get(key));
 					break;
 	
 				case "Libelle_acheminement":
-					commune.setLibelle_acheminement(value);
+					commune.setLibelle_acheminement(listModif.get(key));
 					break;
 	
 				case "Ligne_5":
-					commune.setLigne_5(value);
+					commune.setLigne_5(listModif.get(key));
 					break;
 	
 				case "Latitude":
-					commune.setLatitude(value);
+					commune.setLatitude(listModif.get(key));
 					break;
 	
 				case "Longitude":
-					commune.setLongitude(value);
+					commune.setLongitude(listModif.get(key));
 					break;
 			}
 		}
 	}
+	
+	
+	public static Map<String, String> jsonToMap(String requete) throws JsonParseException, JsonMappingException, IOException { 
+		Map<String, String> dictionnary = new ObjectMapper().readValue(requete, HashMap.class);
+//		System.out.println(dictionnary.get("Code_commune_INSEE"));
+//		System.out.println(dictionnary.keySet());
+//		System.out.println(dictionnary.keySet().toArray()[0]);
+		return dictionnary;
+	}
+	
+	
+	public static String modifVilleBDD(Map<String, String> listModif) {
+		Connection connexion = null;
+		PreparedStatement preparedStatement = null;
+		
+		try {
+			DaoFactory daoFactory = DaoFactory.getInstance();
+			connexion = daoFactory.getConnection();
+			
+			String preparedString = "UPDATE ville_france SET";
+			Object[] listKey = listModif.keySet().toArray();
+			for (Object key : listKey) {
+				if(!key.equals("Code_commune_INSEE")) {
+					preparedString += ", "+key + "='"+listModif.get(key)+"'";
+				}
+			}
+			preparedString = preparedString.replaceFirst(",", "");
+			preparedString += " WHERE Code_commune_INSEE='"+listModif.get("Code_commune_INSEE")+"';";
+
+			preparedStatement = connexion.prepareStatement(preparedString);
+			preparedStatement.executeUpdate();
+			
+			connexion.close();
+		} catch (SQLException e) {
+			return e.getMessage();
+		}
+		
+		return "La commune a bien été modifié";
+	}
+	
+	
+	
+	
+	public static String supprVilleBDD(Map<String, String> listModif) {
+		Connection connexion = null;
+		PreparedStatement preparedStatement = null;
+		
+		try {
+			DaoFactory daoFactory = DaoFactory.getInstance();
+			connexion = daoFactory.getConnection();
+			
+			String preparedString = "DELETE FROM ville_france WHERE ";
+			Object[] listKey = listModif.keySet().toArray();
+			for (Object key : listKey) {
+				if(key.equals("Code_commune_INSEE")) {
+					preparedString += ""+ key + "='"+listModif.get(key)+"';";
+					
+				}
+			}
+
+			if(!preparedString.equals("DELETE FROM ville_france WHERE ")) {
+				preparedStatement = connexion.prepareStatement(preparedString);
+				preparedStatement.executeUpdate();
+			}
+			else {
+				return "La requête n'est pas correct, merci d'utiliser la clé primaire";
+			}
+			
+			
+			connexion.close();
+		} catch (SQLException e) {
+			return e.getMessage();
+		}
+		
+		return "La commune a bien été supprimé";
+	}
+	
+	
+	
 }
